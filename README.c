7dbdcf7e9e6a3650e916cbb1b9918b6dab3982ca
GleisonDamaceno <106999025+GleisonDamaceno@users.noreply.github.com>
2024-03-27 14:32:36 -0300

    Update README.c

--- README.c
+++ README.c
@@ -1,2 +1,10 @@
 # DEV
 #the_future
+#include <stdio.h>
+#include <cs50.h> //Adicionaa biblioteca particular do CS50
+
+int main(void)
+{
+    string nome = get_string("Qual o seu nome? \n"); // get_string é uma função específica da biblioteca do CS50!
+    printf("Hello, %s!\n",nome);
+}
